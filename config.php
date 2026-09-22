9758b1462c704f4893a44038abbfd0e190a790e1
mortada77778-code <mortada77778@gmail.com>
2026-09-22 09:46:29 +0200

    config.php

--- config.php
+++ config.php
@@ -2,17 +2,18 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$host = getenv('DB_HOST') ?: 'mysql.railway.internal';
-$user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASS') ?: 'CoVSqEjqNkjsPSbVrqkVqVgdWdPsgzab';
-$db   = getenv('DB_NAME') ?: 'railway';
-$port = getenv('DB_PORT') ?: '3306';
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
+$port = getenv('DB_PORT');
 
 try {
     $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
     $conn = new PDO($dsn, $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "تم الاتصال بنجاح!";
 } catch (PDOException $e) {
-    die("فشل الاتصال: " . $e->getMessage());
+    echo "خطأ في الاتصال بقاعدة البيانات: " . $e->getMessage();
 }
 ?>
