085ab69a167ee21666cb0d21cf1b9f77b0acb019
mortada77778-code <mortada77778@gmail.com>
2026-09-22 10:28:38 +0200

    config.php

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$host = 'mysql.railway.internal';
+$host = 'yamabiko.proxy.rlwy.net';
 $user = 'root';
 $pass = 'FJMtllwHMAvVsWblAUTcMHoLdvTlnQPk'; 
 $db   = 'railway';
-$port = '3306';
+$port = '28401';
 
 try {
     $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
