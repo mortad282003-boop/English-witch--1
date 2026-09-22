bb36d9c624a7ee28444092d9c0bd0d15c6e4ea8b
mortada77778-code <mortada77778@gmail.com>
2026-09-22 09:28:23 +0200

    config.php

--- config.php
+++ config.php
@@ -8,10 +8,11 @@ $pass = 'CoVSqEjqNkjsPSbVrqkVqVgdWdPsgzab';
 $db   = 'railway';
 $port = 3306;
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
-
-if ($conn->connect_error) {
-    die("فشل الاتصال: " . $conn->connect_error);
+try {
+    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
+    $conn = new PDO($dsn, $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("فشل الاتصال: " . $e->getMessage());
 }
-$conn->set_charset("utf8mb4");
 ?>
